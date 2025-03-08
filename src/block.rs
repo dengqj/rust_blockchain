e6c8d2582135f7f7e2a7a0262680175e7f44d5a5
dengqj <423132414@qq.com>
2025-03-08 15:27:23 +0800

    Update block.rs

--- src/block.rs
+++ src/block.rs
@@ -18,6 +18,7 @@ pub struct Block {
 }
 
 impl Block {
+    //创建新区块
     pub fn new_block(index: u32, data: String, prev_hash: String, nonce: u32) -> Result<Block> {
         let hash = String::new();
         let timestamp: DateTime<Utc> = Utc::now(); 
@@ -33,11 +34,11 @@ impl Block {
         block.validate_block()?;
         Ok(block)
     }
-
+    //创建创世区块
     pub fn new_genesis_block(data: String) -> Block {
         Block::new_block(0, data, String::new(),0).unwrap()
     }
-
+    //验证区块
     fn validate_block(&mut self) -> Result<()> {
         while !self.validate()? {
             self.nonce += 1;
@@ -46,17 +47,18 @@ impl Block {
         self.hash = hash;
         Ok(())
     }
-
+    //验证工作量证明
     fn validate(&self) -> Result<bool> {
         let hash = self.calculate_hash();
         Ok(&hash[0..4] == "0000")
     }
-    
+    //哈希数据处理
     fn hash_data(&self) -> Result<Vec<u8>> {
         let values = (self.index, self.timestamp.timestamp_millis(), &self.data, &self.prev_hash, self.nonce);
         let input = serialize(&values)?;
         Ok(input)
     }
+    //哈希计算
     pub fn calculate_hash(&self) -> String {
         let mut hasher = Sha256::new();
         let input = &self.hash_data().unwrap()[..];
@@ -64,23 +66,23 @@ impl Block {
         let result = hasher.finalize();
         hex::encode(result)
     }
-
+    //提取当前区块哈希
     pub fn get_hash(&self) -> String {
         self.hash.clone()
     }
-
+    //提取前一个区块哈希
     pub fn get_prev_hash(&self) -> String {
         self.prev_hash.clone()
     }
-
+    //提取当前区块索引
     pub fn get_index(&self) -> u64 {
         self.index as u64
     }
-
+    //提取当前区块数据
     pub fn get_data(&self) -> String {
         self.data.clone()
     }
-
+    //提取当前区块创建时间
     pub fn get_timestamp_string(&self) -> String {
         self.timestamp.format("%Y-%m-%d %H:%M:%S UTC").to_string()
     }
